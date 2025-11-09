9c53991cad1596c01387f583de210307f9cc1838
Nikita Yesin <harrybla@student.21-school.com>
2025-11-09 23:18:44 +0300

    part 2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 import psycopg2
+import redis
 import os
 
 app = FastAPI()
@@ -9,6 +10,9 @@ DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "1111")
 DB_NAME = os.getenv("DB_NAME", "visitsdb")
 
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
 def get_db_connection():
     return psycopg2.connect(
         host=DB_HOST,
@@ -17,6 +21,8 @@ def get_db_connection():
         dbname=DB_NAME
     )
 
+def get_redis_connection():
+    return redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 
 @app.get("/ping")
@@ -28,14 +34,23 @@ def ping(request: Request):
     conn.commit()
     cur.close()
     conn.close()
+    
+    r = get_redis_connection()
+    r.incr("total_visits")
+
     return {"message": "pong"}
 
 @app.get("/visits")
 def visits():
-    conn = get_db_connection()
+    r = get_redis_connection()
+    cached = r.get("total_visits")
+    if cached is not None:
+        return {"visits": int(cached)}
+        conn = get_db_connection()
     cur = conn.cursor()
     cur.execute("SELECT COUNT(*) FROM visits;")
     count = cur.fetchone()[0]
     cur.close()
     conn.close()
+    r.set("total_visits", count)
     return {"visits": count}
